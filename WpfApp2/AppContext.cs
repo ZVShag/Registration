fbb963e427de95ed38c7e34652710e7e40ff9431
Alexey Alexey <alextutor72@gmail.com>
2023-03-01 18:46:37 +0500

    final

--- WpfApp2/AppContext.cs
+++ WpfApp2/AppContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Entity;
 
 namespace WpfApp2
 {
-    internal class AppContext
+    internal class AppContext: DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public AppContext() : base("database.db") { }
     }
 }
