a654ca3f237d58672188ebe85d491834531ebffc
Alexey Alexey <alextutor72@gmail.com>
2023-03-15 17:45:47 +0500

    1745

--- WpfApp2/User.cs
+++ WpfApp2/User.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace WpfApp2
 {
@@ -26,8 +27,44 @@ namespace WpfApp2
         {
             string user_info=this.Login+" "+this.Email+ " "+this.Password;
             StreamWriter f=new StreamWriter(this.path,true);
-            f.Write(user_info);
+            f.WriteLine(user_info);
             f.Close();
+            //Shifr();
         }
+        public void Shifr()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                byte[] salt = new byte[16];
+                new RNGCryptoServiceProvider().GetBytes(salt);
+
+                Rfc2898DeriveBytes keyGenerator = new Rfc2898DeriveBytes("password", salt);
+                aes.Key = keyGenerator.GetBytes(aes.KeySize / 8);
+
+                aes.IV = GenerateRandomBytes(aes.BlockSize / 8);
+
+                using (FileStream inputFileStream = new FileStream(path, FileMode.Open))
+                using (FileStream outputFileStream = new FileStream(path, FileMode.Create))
+                using (CryptoStream cryptoStream = new CryptoStream(outputFileStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    outputFileStream.Write(salt, 0, salt.Length);
+
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = inputFileStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        cryptoStream.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+        static byte[] GenerateRandomBytes(int length)
+        {
+            byte[] bytes = new byte[length];
+            new RNGCryptoServiceProvider().GetBytes(bytes);
+            return bytes;
+        }
+    
     }
 }
